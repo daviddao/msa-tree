da863bf96d55f2d52f95d98033aa9641eb49457d
Sebastian Wilzbach <sebi@wilzbach.me>
2014-12-10 20:32:51 +0100

    trivial testing

--- test/testModel.js
+++ test/testModel.js
@@ -5,21 +5,27 @@ var fs = require("fs");
 var fasta = require("biojs-io-fasta").parse;
 var newick = require("biojs-io-newick");
 
-//var model = require("../lib/model");
+var model = require("../lib/model");
 require("./mochaFix.js"); // fix for solarized dark
 
 describe("Model", function() {
+  var g;
   beforeEach(function(){
     var fastaSeq = fs.readFileSync(__dirname + "/dummy/dummy_msa.fasta", "utf8");
     var seqs = fasta.parse(fastaSeq);
     var newickRaw = fs.readFileSync( __dirname + "/dummy/dummy_newick.newick", "utf8");
     var treeData = newick.parse_newick(newickRaw);
-    console.log(seqs);
-    console.log(treeData);
+    seqs.map(function(el){
+      // parseInt ? 
+      el.id = el.name.split("|")[0];
+    });
+    g = new model.childs(seqs);
   });
   describe("constructing", function() {
     it("constructing", function() {
 
+    equal(4,g.length);
+    equal("ACGACGAACT",g.at(0).attributes.data.attributes.seq);
 
 
     });
