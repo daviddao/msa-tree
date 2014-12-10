8a4ce204e0579d10659033929a64861d9afd5939
Sebastian Wilzbach <sebi@wilzbach.me>
2014-12-11 00:27:38 +0100

    use parcelify for css dependencies

--- examples/first.js
+++ examples/first.js
@@ -1,7 +1,9 @@
 /* global yourDiv */
 var msaDiv = document.createElement("div");
+var menuDiv = document.createElement("div");
 var treeDiv = document.createElement("div");
 
+yourDiv.appendChild(menuDiv);
 yourDiv.appendChild(msaDiv);
 yourDiv.appendChild(treeDiv);
 
@@ -9,11 +11,14 @@ var combi = require("msa-tnt");
 var model = combi.model;
 var adapters = combi.adapters;
 var fasta = require("biojs-io-fasta").parse;
+var msa = require("biojs-vis-msa");
 
 fasta.read("../test/dummy/dummy_msa.fasta", function(seqs){
   var nodes = new model.nodes(seqs);
   console.log(nodes);
-  var m = new adapters.msa({model: nodes, el: msaDiv});
+  var m = new adapters.msa({model: nodes, el: msaDiv, colorscheme: {scheme: "clustal"}});
   m.render();
+  var menu = new msa.menu.defaultmenu({el: menuDiv, msa: m.msa});
+  menu.render();
 });
 
