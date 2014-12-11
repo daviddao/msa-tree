9f16ab6e7ef615d80210aa6cbd38399735c928d5
Sebastian Wilzbach <sebi@wilzbach.me>
2014-12-11 01:13:04 +0100

    selection events

--- examples/first.js
+++ examples/first.js
@@ -10,15 +10,37 @@ yourDiv.appendChild(treeDiv);
 var combi = require("msa-tnt");
 var model = combi.model;
 var adapters = combi.adapters;
+var selections = combi.selections;
 var fasta = require("biojs-io-fasta").parse;
+var newick = require("biojs-io-newick").parse_newick;
 var msa = require("biojs-vis-msa");
 
-fasta.read("../test/dummy/dummy_msa.fasta", function(seqs){
+var sel = new selections();
+
+combi.utils.xhr(["../test/dummy/dummy_msa.fasta", "../test/dummy/dummy_newick.newick"]).then(function(result) {
+  var seqs = fasta.parse(result[0]);
+
+  var n = newick(result[1]);
+  console.log(n);
+
   var nodes = new model.nodes(seqs);
   console.log(nodes);
-  var m = new adapters.msa({model: nodes, el: msaDiv, colorscheme: {scheme: "clustal"}});
-  m.render();
-  var menu = new msa.menu.defaultmenu({el: menuDiv, msa: m.msa});
-  menu.render();
-});
 
+  var m = new adapters.msa({
+    model: nodes,
+    el: msaDiv,
+    sel: sel,
+    colorscheme: {
+      scheme: "clustal"
+    }
+  });
+  m.render();
+
+  var menu = new msa.menu.defaultmenu({
+    el: menuDiv,
+    msa: m.msa
+  });
+  menu.render();
+}, function(error) {
+  console.error(error);
+});
